73c847c20d704a716bb1d2661a7d4305271edd55
leesh815 <leesh815@nc-and.com>
2026-03-01 13:50:08 +0900

    add key mapping for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,16 @@
+local keymap = require("utils.keyMapper")
 return {
     'nvim-telescope/telescope.nvim', version = '0.1.9',
     dependencies = {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      keymap.nmap("<leader>ff", builtin.find_files)
+      keymap.nmap("<leader>fg", builtin.live_grep)
+      keymap.nmap("<leader>fb", builtin.buffers)
+      keymap.nmap("<leader>fh", builtin.help_tags)
+    end
 }
