7a26afae3888c9fa51dea135349097fb516b289b
leesh815 <leesh815@nc-and.com>
2026-05-01 19:03:33 +0900

    Fix telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,30 @@
-local keymap = require("utils.keyMapper")
 return {
-    'nvim-telescope/telescope.nvim', version = '0.1.9',
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.9",
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
     },
     config = function()
+      local telescope = require("telescope")
       local builtin = require("telescope.builtin")
-      keymap.nmap("<leader>ff", builtin.find_files)
-      keymap.nmap("<leader>fg", builtin.live_grep)
-      keymap.nmap("<leader>fb", builtin.buffers)
-      keymap.nmap("<leader>fh", builtin.help_tags)
+      local keymap = require("utils.keyMapper")
+
+      telescope.setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+
+      telescope.load_extension("ui-select")
+
+      keymap.nmap("<leader>ff", builtin.find_files, { desc = "Find files" })
+      keymap.nmap("<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      keymap.nmap("<leader>fb", builtin.buffers, { desc = "Buffers" })
+      keymap.nmap("<leader>fh", builtin.help_tags, { desc = "Help tags" })
     end,
-    {
-      'nvim-telescope/telescope-ui-select.nvim',
-      config = function()
-        require("telescope").setup {
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown {
-              }
-            }
-          }
-        }
-        require("telescope").load_extension("ui-select")
-      end
-    }
+  },
 }
